0ab1729e4af0c65b6a2f80ff0fbd6fb5275e0165
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-26 01:56:53 -0700

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a file in the /tmp directory
-file { '/tmp/holberton':
+file { '/tmp/school':
   ensure  => 'file',
-  mode    => '0744',
+  permission    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
