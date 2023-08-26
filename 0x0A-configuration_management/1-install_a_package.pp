4cbe21c66e4c3219bba4028291fd987262cff8bc
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-26 21:15:07 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,8 @@
 # installs the package flask using puppet
 package { 'flask':
-  ensure          => installed, version:'2.1.0',
+  ensure          => version:'2.1.0',
   provider        => 'pip3',
-  install_options => [
-    { '--index-url' => 'https://pypi.mycorp.com'
-    }]
+  
 
  
 }
