148a510fe8acf1ab84d48e5cc8c17dc9a27f727d
tessykc <tessykc@gmail.com>
2023-09-04 11:18:47 -0700

    0x0F-load_balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,5 @@
+# automate the task of creating a custom HTTP header response, but with Puppet.
+# The name of the custom HTTP header must be X-Served-By
+# The value of the custom HTTP header must be the hostname of the server Nginx
+# is running on
+
