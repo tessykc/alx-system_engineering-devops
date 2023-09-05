fd332cf253921684f08dd4ff05776498a9d39ed6
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-09-05 04:34:00 -0700

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,3 +3,22 @@
 # The value of the custom HTTP header must be the hostname of the server Nginx
 # is running on
 
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx']
+  }
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx'
+  before   => Exec['add_header'],
+  }
+exec {'add_header':
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+exec {'restart Nginx':
+  provider => shell,
+  command  => 'sudo service Nginx restart',
+  }
