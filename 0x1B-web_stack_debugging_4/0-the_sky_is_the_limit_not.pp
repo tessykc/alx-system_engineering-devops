16a443381c0b5664e72c17bab1e4483ed56b4a8c
tessykc <tessykc@gmail.com>
2024-03-17 13:55:23 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,26 +1,23 @@
-# Define class to manage Nginx configuration
-class nginx_module::nginx_config {
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure => installed,
+}
 
-  # Ensure Nginx package is installed
-  package { 'nginx':
-    ensure => installed,
-  }
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure => running,
+}
 
-  # Define Nginx configuration file
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('nginx_module/nginx.conf.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Adjust Nginx configuration to increase its capacity
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('path/to/nginx.conf.erb'), # Path to your nginx configuration template
+  notify  => Service['nginx'],
+}
 
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
+# Define custom Nginx configuration template
+file { 'path/to/nginx.conf.erb':
+  ensure  => file,
+  content => template('module_name/nginx.conf.erb'), # Path to your custom Nginx configuration template
+  notify  => Service['nginx'],
 }
